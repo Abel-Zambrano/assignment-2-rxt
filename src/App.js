509338d86da2dab63e0b1b91456745cffe12a840
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-10-21 12:06:11 -0700

    added delete method

--- src/App.js
+++ src/App.js
@@ -4,18 +4,29 @@ import Validation from './components/Validation';
 import Char from './components/Char';
 
 function App() {
+// STATE ==========================================================================
   const [state, setState] = useState({
     userInput: ''
   });
 
+// Handler ========================================================================
   const inputHandler = (event) => {
     setState({userInput: event.target.value});
   }
 
+  const deleteCharHandler = (index) => {
+    const text = state.userInput.split('');
+    text.splice(index, 1);
+    const updatedText = text.join('');
+    setState({userInput: updatedText});
+  }
+
+// MAP ============================================================================
   const charList = state.userInput.split('').map((ch, index) => {
-    return <Char key={index} character={ch} />
+    return <Char key={index} character={ch} clicked={() => deleteCharHandler(index)} />
   });
 
+// RENDER =========================================================================
   return (
     <div className="App">
      <h1>Assignment 2</h1>
