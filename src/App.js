1635e6dd58e46a44beff0656da0e9e4a52051d7e
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-10-21 11:03:55 -0700

    added component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [state, setState] = useState({
+    userInput: ''
+  });
+
+  const inputHandler = (event) => {
+    setState({userInput: event.target.value});
+  }
+
   return (
     <div className="App">
      <h1>Assignment 2</h1>
+     <input type="text" onChange={inputHandler} value={state.userInput} />
+     <p>{state.userInput}</p>
     </div>
   );
 }
